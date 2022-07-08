693a0f6017e577dc4b334e9575b008cfa5d9de18
vominhtrieu <minhtrieuvo600@gmail.com>
2022-07-08 09:27:23 +0700

    Fix toggle mobile UI

--- components/MainNavbar.js
+++ components/MainNavbar.js
@@ -23,8 +23,8 @@ export default function MainNavbar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#about">Experiences</Nav.Link>
+                        <Nav.Link href="#education">Education</Nav.Link>
+                        <Nav.Link href="#experiences">Experiences</Nav.Link>
                         <Nav.Link href="#skills">Skills</Nav.Link>
                         <Nav.Link href="#projects">Projects</Nav.Link>
                         <Nav.Link href="#contacts">Contact</Nav.Link>
