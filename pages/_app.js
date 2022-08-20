dd5cd0da6993bcc62c9d2abf463c8f4d7b49ede5
trieuplacement <trieu.vo@placement.com>
2022-08-20 15:08:16 +0700

    Added Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/globals.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics gaMeasurementId="G-5GFVZV2DZ5" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
